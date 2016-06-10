e9e6c905b9c81171eb7c8fcc75d0c2d53b821071
Chi-Han Huang <huangchiheng@gmail.com>
2016-06-10 14:23:17 +0800

    remove unnecessary test files

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import test from './cat'
-console.log('nihao')
-// import cat from './cat.js'
-// var cat = 'cat'
-// console.log(cat)
-// var cats = require('./cat.js');
-// console.log(cat);
--- src/cat.js
+++
@@ -1,5 +0,0 @@
-export default function test () {
-  return 'hello world'
-}
-// var cats = ['dave', 'henry', 'martha'];
-// module.exports = cats;
