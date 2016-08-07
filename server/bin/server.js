8be9946512317b8cb88c998f6522e2d693f0a0af
Huang Chi-Han <huangchiheng@gmail.com>
2016-08-08 00:08:09 +0800

    1. move bin to root level 2. remove unnecessary file

--- server/bin/server.js
+++
@@ -1,8 +0,0 @@
-const fs =  require('fs')
-const path = require('path')
-const config =  JSON.parse(fs.readFileSync(path.resolve(__dirname, '../..', '.babelrc'), 'utf-8'))
-require('babel-register')(config)
-require('css-modules-require-hook')({
-  generateScopedName: '[name]__[local]___[hash:base64:5]',
-})
-require(path.resolve(__dirname, '../server.js'))
