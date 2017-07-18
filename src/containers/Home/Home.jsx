21e531860590b2d663e7712ba6a7c8343c857ae2
Huang Chi-Han <huangchiheng@gmail.com>
2017-07-18 16:03:24 +0800

    Remove usage of Title component.

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import Title from '../../components/Title'
 import styles from './Home.css'
 
 const Home = () => (
 	<div>
-		Start writing some react!!!
+		Start writing some react!
 		<i className="fa fa-smile-o" aria-hidden="true"></i>
 	</div>
 )
